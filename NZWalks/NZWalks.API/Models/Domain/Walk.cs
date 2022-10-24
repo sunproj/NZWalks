1550ef21b9eccf6bb6564659b5bc64f414f23413
sunilgit81 <103314896+sunilgit81@users.noreply.github.com>
2022-10-24 08:48:37 +0530

    End of Section2 - Wbapi Core

---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,21 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public long Length { get; set; }
+
+        public Guid WalkDifficultyId { get; set; }
+
+        public Guid RegionId { get; set; }
+
+        //Navigation Property
+
+        public Region Region{ get; set; }
+
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,9 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid WalkDifficultyId { get; set; }
+
+        public string Code { get; set; }
+    }
+}
